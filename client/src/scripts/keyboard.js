88cc27254367bf63698138a9a0612f56a1e199e1
Matthew Huxtable <git@tigermatt.co.uk>
2018-10-20 22:10:17 +0100

    Escape key can abort event flow when in debug mode

--- client/src/scripts/keyboard.js
+++ client/src/scripts/keyboard.js
@@ -4,6 +4,7 @@ export const KEY_UP = 38;
 export const KEY_LEFT = 37;
 export const KEY_RIGHT = 39;
 export const KEY_SPACE = 32;
+export const KEY_ESCAPE = 27;
 
 
 export class KeyboardEventHandler {
