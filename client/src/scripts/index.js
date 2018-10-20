69d32adfc9c699ae254edd6a064b3a999ccd22a3
Humphrey Shotton <humphrey.shotton@sparx.co.uk>
2018-10-20 12:08:04 +0100

    make eslint work

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,11 +1,15 @@
 import '../styles/index.scss';
-import * as PIXI from 'pixi.js'
+import { Application } from 'pixi.js';
 
-var app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+let app = new Application(
+    document.body.clientWidth,
+    document.body.clientHeight,
+    {backgroundColor : 0x1099bb}
+);
 document.body.appendChild(app.view);
 
 // create a new Sprite from an image path
-var bunny = PIXI.Sprite.fromImage('public/assets/spx_X_white.jpg');
+let bunny = PIXI.Sprite.fromImage('public/assets/spx_X_white.jpg');
 
 // center the sprite's anchor point
 bunny.anchor.set(0.5);
